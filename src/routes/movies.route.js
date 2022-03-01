92fc200e0e9501e077a2b0e3c867d7f316864b47
ismailaissami <aissami.ismail@gmail.com>
2022-03-01 21:24:19 +0100

    save point

--- src/routes/movies.route.js
+++ src/routes/movies.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const movie = require('../controllers/movie.controller');
 
 router.get('/movie/:id', movie.getMovie);
-router.get('/movies/', movie.getMovies);
+router.get('/getMovies/', movie.getMovies);
 router.post('/createMovie', movie.createMovie);
 router.post('/edit/:id', movie.updateMovie);
 router.delete('/delete/:id', movie.removeOne);
